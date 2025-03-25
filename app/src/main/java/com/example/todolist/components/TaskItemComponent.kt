5a70a996ece1682f6d8d416377e726fb40c3041b
MarkPolyakov <markpolya@icloud.com>
2025-03-25 15:33:51 +0500

    Сделал карточку задачи с кнопкой удаления

---
+++ app/src/main/java/com/example/todolist/components/TaskItemComponent.kt
@@ -0,0 +1,35 @@
+package com.example.todolist.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.todolist.models.Task
+
+@Composable
+public fun TaskItemComponent(task: Task, onDelete: () -> Unit) {
+    Card(modifier = Modifier.fillMaxWidth().padding(4.dp),
+        elevation = CardDefaults.cardElevation(4.dp))
+    {
+        Row(modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween)
+        {
+            Text(text=task.title, modifier = Modifier.weight(1f))
+            IconButton(onClick = onDelete) {
+                Icon(Icons.Default.Delete, contentDescription = "Delete")
+            }
+        }
+    }
+}
